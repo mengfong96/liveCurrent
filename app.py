5d00074978d07cf9776656c5bafb52d878d7a411
Lee Meng Fong <leemengfong.96@gmail.com>
2020-03-07 11:33:38 +0800

    Initial Commit

---
+++ app.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template
+from flask import request
+import pickle
+import pandas as pd
+import numpy as np
+
+
+app = Flask(__name__)
+
+df = pd.read_csv("Fyp Demo.csv")
+df["Datetime"] = pd.to_datetime(df["Date"] + " " + df["Time"])
+df = df.set_index("Datetime")
+
+log_reg1 = pickle.load(open("log_reg1(ML).pkl", "rb"))
+log_reg2 = pickle.load(open("log_reg2(ML).pkl", "rb"))
+log_reg3 = pickle.load(open("log_reg3(ML).pkl", "rb"))
+scaler,onehot = pickle.load(open("preprocessor(ML).pkl", "rb"))
+
+def get_prediction(matched_row):
+    s1 = log_reg1.predict(matched_row)
+    s2 = log_reg2.predict(matched_row)
+    s3 = log_reg3.predict(matched_row)
+    return s1, s2, s3
+
+def get_matched_row(user_time):
+    user_time = pd.date_range('17-12-2006', periods = 720, freq ='T') 
+    index_user_time = df.loc[user_time]
+    
+    features = index_user_time[["Power Factor", "Voltage", "Current"]].values
+#    features = features.reshape(1,-1)
+    features = scaler.transform(features)
+    
+    rp = onehot.transform(index_user_time["Real Power"].values.reshape(-1, 1))
+    features = np.concatenate([features, rp[:, 1:]], axis=-1)
+    return features
+
+def get_real_power(power):
+    user_time = pd.date_range('17-12-2006', periods = 720, freq ='T') 
+    index_user_time = df.loc[user_time]
+    
+    power_feature = index_user_time[["Real Power"]].values
+    return power_feature
+
+def get_cursor_point(x_point):
+    user_time = pd.date_range('17-12-2006', periods = 720, freq ='T') 
+    index_user_time = df.loc[user_time]
+    power_feature = index_user_time[["Real Power"]].values
+    power_feature = power_feature.reshape(-1,)
+    
+    features_for_ML = get_matched_row(user_time)
+    s1, s2, s3 = get_prediction(features_for_ML)
+    
+    print(s1.shape, power_feature.shape)
+    cursor_point = np.concatenate([power_feature, s1, s2, s3], axis=-1)
+    cursor_point_value = cursor_point
+    
+    return cursor_point_value
+
+@app.route('/')
+def html_table():
+    legend = 'Power Usage'
+    labels =  pd.date_range('17-12-2006', periods = 720, freq ='T')
+    values =  get_cursor_point(labels)   
+    
+    return render_template('Energy_Consumption_Graph.html', values=values, labels=labels, legend=legend)
+
+@app.route('/chart-data')
+def chart_data(): 
+
+    df
+
+    return Response(generate_random_data(), mimetype='text/event-stream')
+    
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ static/app.js
@@ -0,0 +1,50 @@
+$(document).ready( function() { 
+
+    $("#btnStart").click(function(){ 
+        //make it event lister
+        //alert("You have click 9 the Start button...");
+    });
+
+    $("#btnCancel").click(function(){ 
+        //make it event lister
+        //alert("You have click 9 the Cancel button...");
+    });
+
+    setInterval(function(){myTimer()},1000);
+    setInterval(function(){updateValues()},1000); 
+
+}); 
+
+var startTime = new Date(); 
+
+function updateValues(){ 
+
+alert("load me? ")
+}
+
+function myTimer(){ 
+    var myTime = new Date(2020,1,1,12,00,00,00); 
+    var diff = new Date() - startTime; 
+
+    myTime.setMilliseconds(myTime.getMilliseconds()+ diff); 
+
+    var hours=myTime.getHours();
+    var minutes=myTime.getMinutes();
+    var seconds=myTime.getSeconds();
+    
+    if(minutes < 10){ 
+        minutes="0" + minutes; 
+    }
+    if(seconds < 10) { 
+        seconds="0" + seconds; 
+    }
+    var outputDateTime = hours+":"+minutes+":"+seconds; 
+    document.getElementById("dateTime").innerHTML = outputDateTime;  //d.toLocaleTimeString();
+
+    var outputDate = startTime.toLocaleDateString(); 
+    document.getElementById("date").innerHTML = outputDate;
+
+}
+
+
+
